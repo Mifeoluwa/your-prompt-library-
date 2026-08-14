f0472be13d927cdd78f7a2a07af5edd33989d733
Akinola Moses <Mifeoluwa>
2026-08-14 10:57:15 +0100

    Add assignment.py

---
+++ Assignment.py
@@ -0,0 +1,8 @@
+numbers = [349, 245, 223, 33, 334, 96, 406, 49, 23]
+
+minimum = min(numbers)
+maximum = max(numbers)
+
+print("List:", numbers)
+print("Minimum number:", minimum)
+print("Maximum number:", maximum)
